d9711bfa55d992537328299504232dab38382ea9
Caio <caiolucasodev@gmail.com>
2026-07-23 21:29:54 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,17 @@
+ const users = [
+    {
+        name: "Carlos",
+        age: 30,
+        status: true
+    },{
+        name: "Carol",
+        age: 22,
+        status: false
+    },{
+        name: "Ceci",
+        age: 2,
+        status: true
+    }
+];
+
+export default users
