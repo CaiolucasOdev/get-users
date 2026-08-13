4b3179233dc9c0aecd695cd2f234c747650013ed
Caio <caiolucasodev@gmail.com>
2026-08-13 18:19:06 -0300

    feat: alteração de logica para busca de usuarios

--- client/app.js
+++ client/app.js
@@ -5,15 +5,15 @@ const resultado = document.querySelector('h1');
 const resource = 'users';
 
 async function getUsers() {
-    const id = Number(input.value);    const response = await fetch(`http://localhost:3000/${resource}`);
-
-    if (!response.ok) {
-        resultado.textContent = 'Erro ao buscar usuários.';
+    const id = Number(input.value);  //converte o valor dos input's em valores numericos
+    const response = await fetch(`http://localhost:3000/${resource}`)
+    if (!response.ok) {//se a resposta do servidor for diferente de ok
+        resultado.textContent = 'Erro ao buscar usuários.';//altera o conteudo do h1 ao não encontrar o id de usuario
         return;
     }
 
     const data = await response.json();
-    const user = data.find(user => user.id === id);
+    const user = data.find(user => user.id === id);//captura os valores de id restritos de usuário
 
     if (!user) {
         resultado.textContent = `Usuário de Id ${id} não encontrado`;
