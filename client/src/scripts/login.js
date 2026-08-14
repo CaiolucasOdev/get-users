8a0d0a3a9a16be90cd88da2c77a4a8b394c3b5f0
Caio <caiolucasodev@gmail.com>
2026-08-13 21:51:03 -0300

    adcionar pagina de login

--- client/src/scripts/login.js
+++ client/src/scripts/login.js
@@ -1,5 +1,7 @@
 const email = document.querySelector('input[type="email"]');
 const password = document.querySelector('input[type="password"]');
+const checkbox = document.querySelector('input[type="checkbox"]')
+const labelPassword = document.querySelector('#placa')
 const button = document.querySelector('button');
 
 
@@ -26,4 +28,19 @@ button.addEventListener('click',(event) => {
    alert('Acesso permitido!')
 
 
+})
+
+checkbox.addEventListener('change', () =>{
+   const AttributeValue = password.getAttribute('type')
+
+   if ( AttributeValue === 'password'){
+    password.setAttribute('type', 'text');
+    labelPassword.textContent = 'Ocultar senha'
+    return
+   }
+
+   
+   password.setAttribute('type', 'password');
+   labelPassword.textContent = 'Mostrar senha'
+   
 })
